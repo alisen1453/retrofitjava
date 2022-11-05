19179d3839baeec2bd20d89ed27d3b2de1aac26f
Bayram Ali ŞEN <110849653+alisen1453@users.noreply.github.com>
2022-11-05 13:10:53 +0300

    Rx java and Retrofit

--- app/src/main/java/com/example/retrofitjava/sevices/serviceapi.java
+++ app/src/main/java/com/example/retrofitjava/sevices/serviceapi.java
@@ -4,10 +4,12 @@ import com.example.retrofitjava.model.Model;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface serviceapi {
     @GET("posts")
-    Call<List<Model>> getData();
+    Observable <List<Model>> getData();
+    //Call<List<Model>> getData();
 }
--- app/src/main/java/com/example/retrofitjava/view/MainActivity.java
+++ app/src/main/java/com/example/retrofitjava/view/MainActivity.java
@@ -19,10 +19,15 @@ import com.google.gson.GsonBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     Retrofit retrofit;
     private apiadapter apiadapter;
     private ActivityMainBinding binding;
+    CompositeDisposable compositeDisposable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +44,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
         //models = new ArrayList<>();
         Gson gson = new GsonBuilder().setLenient().create();
-        retrofit = new Retrofit.Builder().baseUrl(constants.BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(constants.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
         loadData();
     }
     private void loadData() {
+        serviceapi services = retrofit.create(serviceapi.class);
+        compositeDisposable=new CompositeDisposable();
+        compositeDisposable.add(services.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::getDataObservable));
+        /*
         serviceapi services = retrofit.create(serviceapi.class);
         Call<List<Model>> call = services.getData();
         call.enqueue(new Callback<List<Model>>() {
@@ -51,11 +68,11 @@ public class MainActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     List<Model> list = response.body();
                     models = new ArrayList<>(list);
-                      /*
-                    for (Model model:models){
-                        System.out.println(model.title);
-                    }
-                     */
+
+                   // for (Model model:models){
+                    //    System.out.println(model.title);
+                   // }
+
                     binding.recyvleview.setLayoutManager(new LinearLayoutManager(MainActivity.this));
                     apiadapter = new apiadapter(models);
                     binding.recyvleview.setAdapter(apiadapter);
@@ -65,6 +82,24 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(Call<List<Model>> call, Throwable t) {
                 t.printStackTrace();
             }
-        });
+        });*/
+    }
+    public void getDataObservable(List<Model> list){
+        models = new ArrayList<>(list);
+
+        // for (Model model:models){
+        //    System.out.println(model.title);
+        // }
+
+        binding.recyvleview.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        apiadapter = new apiadapter(models);
+        binding.recyvleview.setAdapter(apiadapter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
